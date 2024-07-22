be6443906b77940a6e918ee749d25c03ad3293bd
Kieran Barnes <104922102+keabre@users.noreply.github.com>
2024-07-22 19:15:54 +0100

    Create conditional rrouting template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react"
 import HomepageLoggedOutPage from "./components/pages/HomepageLoggedOutPage"
 import { Route, Routes } from "react-router-dom"
+import HomepageLoggedInPage from "./components/pages/HomepageLoggedInPage"
 
 export default function App() {
-
+  const [state, setState] = useState(true)
   return (
-
     <>
-      <Routes>
-        <Route path="/" element={<HomepageLoggedOutPage />}/>
-      </Routes>
+      {state ? (
+          <Routes>
+            <Route path="/" element={<HomepageLoggedOutPage />} />
+          </Routes> 
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomepageLoggedInPage />} />
+          </Routes>
+        )
+      }
+      
     </>
   )
 }
