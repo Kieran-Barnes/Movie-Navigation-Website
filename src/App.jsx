7d651a3a678799bcc01840688ee4815cce4d568d
Kieran Barnes <104922102+keabre@users.noreply.github.com>
2024-07-22 22:00:38 +0100

    Add organisational comments for imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+// import components
 import HomepageLoggedOutPage from "./components/pages/HomepageLoggedOutPage"
-import { Route, Routes } from "react-router-dom"
 import HomepageLoggedInPage from "./components/pages/HomepageLoggedInPage"
+
+// import context 
 import { UserContext } from "./context/UserContext"
+
+// import external utilities
+import { Route, Routes } from "react-router-dom"
 import { useContext } from "react"
 
 export default function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+// import external utilities
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+
+// import components
+import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
