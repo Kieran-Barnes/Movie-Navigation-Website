5b8037483c3f4793eb52e0435a8f3710d2519172
Kieran Barnes <104922102+keabre@users.noreply.github.com>
2024-07-23 15:20:07 +0100

    Setup input field component to accept submit state
    allow the UsernamePassordInputField to accept a on change listener that sets the value of the username in context and also allow the component to accept the same states' value as the input fields' value

--- src/components/common/UsernamePasswordInputField.jsx
+++ src/components/common/UsernamePasswordInputField.jsx
@@ -1,9 +1,15 @@
-export default function UsernamePasswordInputField({ inputType, labelName, tagPrefix }) {
+export default function UsernamePasswordInputField({ inputType, labelName, tagPrefix, inputValue, onChangeFunc }) {
     return (
         <>
             <div>
                 <label htmlFor={`${tagPrefix}-input-field`}>{labelName}</label>
-                <input type={inputType} name={`${tagPrefix}-input-field`} id={`${tagPrefix}-input-field`}/>
+                <input 
+                    type={inputType} 
+                    value={inputValue} 
+                    onChange={onChangeFunc} 
+                    name={`${tagPrefix}-input-field`} 
+                    id={`${tagPrefix}-input-field`}
+                />
             </div>
         </>
     )
