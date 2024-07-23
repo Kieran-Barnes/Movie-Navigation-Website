83be7a706bb0114b691823f9b61b697c08a91fe2
Kieran Barnes <143276969+Kieran-Barnes@users.noreply.github.com>
2024-07-24 00:26:03 +0100

    Link context to LogoutConfirmationPage
    also create button on HomepageLoggedIn to allow user to navigate to LogoutConfirmationPage

--- src/components/pages/HomepageLoggedInPage.jsx
+++ src/components/pages/HomepageLoggedInPage.jsx
@@ -1,9 +1,14 @@
+// import external utilities 
+import { Link } from "react-router-dom"
+
+// import components
 import HomepageTemplate from "../common/HomepageTemplate"
 
 export default function HomepageLoggedInPage() {
     return (
         <>
-            <HomepageTemplate ifLoggedInAccDisplay={"Your Account"} linkPath={'/your-account'}/> 
+            <HomepageTemplate ifLoggedInAccDisplay={"Your Account"} linkPath={'/your-account'}/>
+            <Link to="/logout"><button>Log out</button></Link> 
         </>
     )
 }
--- src/components/pages/LogoutConfirmationPage.jsx
+++ src/components/pages/LogoutConfirmationPage.jsx
@@ -1,10 +1,16 @@
+// import external utilities
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+// import context
+import { UserContext } from "../../context/UserContext";
 
 export default function LogoutConfirmationPage() {
+    const { logout } = useContext(UserContext)
     return (
         <>
             <h1>Are you sure you want to log out?</h1>
-            <button>Yes. Log out</button>
+            <button onClick={logout}>Yes. Log out</button>
             <Link to="/"><button>No. Take me home</button></Link>
         </>
     )
