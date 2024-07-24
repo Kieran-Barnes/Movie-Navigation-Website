b7102d2cf0cb0d647fe7cf475d368d93d0a6acaf
Kieran Barnes <143276969+Kieran-Barnes@users.noreply.github.com>
2024-07-24 11:05:34 +0100

    Modify YourAccountPage to dynamically include username

--- src/components/pages/YourAccountPage.jsx
+++ src/components/pages/YourAccountPage.jsx
@@ -1,14 +1,24 @@
+// import components
 import BackToHomeButton from "../common/BackToHomeButton";
 
+// import context
+import { UserContext } from "../../context/UserContext";
+
+// import external utilities 
+import { useContext } from "react";
+
+
 export default function YourAccountPage() {
+    const { username } = useContext(UserContext)
+
     return (
         <>
-            <h1>{`Hello, user`}</h1>
+            <h1>{`Hello, ${username}`}</h1>
             {/* 'user' in the template literal will be replaced by dynamically determined username */}
             <BackToHomeButton />
             <section>
                 <ul>
-                    <li>Username: username</li>
+                    <li>Username: {username}</li>
                     <li>Date Of Birth: --/--/--</li>
                     <li>Account Created: 23rd July 2024</li>
                     {/* Add more when logic for the user is implemented */}
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -73,6 +73,7 @@ function UserContextProvider({ children }) {
 
     const contextValues = {
         userLoggedIn,
+        username,
         ...loginContext,
         ...signupContext,
         logout
